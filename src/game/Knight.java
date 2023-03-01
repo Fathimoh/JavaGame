2bf5163f774a7183dbf79bc4b0f0d87399c03c24
Fathi-Moh <Fathi.Mohamed@city.ac.uk>
2023-03-01 18:33:27 +0000

    got rid of some images and created a knight class which extends from DynamicBody.I encapsulated the character knight from the java game class.

--- src/game/Knight.java
+++ src/game/Knight.java
@@ -1,5 +1,14 @@
 package game;
 
-public class Knight {
+import city.cs.engine.*;
 
+public class Knight extends DynamicBody {
+    private static final Shape KnightShape = new BoxShape(1,2);
+
+    private static final BodyImage image = new BodyImage("data/Freeknight/MC/Gifs/Idle.gifs", 4f);
+
+    public Knight(World world){
+        super(world, KnightShape);
+        addImage(image);
+    }
 }
